08dbc60d616df3b723e46e812263ce8856c24984
Ravi Siddarth <ravi@hashcooki.es>
2016-12-27 15:20:56 +0530

    Replace boilerplate generated content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import './Header.scss'
+import { Navbar , Nav , NavItem } from 'react-bootstrap'
+import { IndexLinkContainer , LinkContainer } from 'react-router-bootstrap'
+import Logo from 'assets/images/hash-invoices-logo.png'
+
 
 export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
+  <div className="header-nav">
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Toggle />
+
+        <Navbar.Brand>
+           <IndexLink to='/' >
+             <img alt="Brand" src={Logo} />
+           </IndexLink>
+        </Navbar.Brand>
+
+      </Navbar.Header>
+
+      <Navbar.Collapse>
+         <Nav bsStyle="pills" pullRight >
+            <LinkContainer to='/contactus' >
+              <NavItem eventKey={1} > Contact Us </NavItem>
+              </LinkContainer>
+            <LinkContainer to='/aboutus' >
+              <NavItem eventKey={2} >About Us</NavItem>
+            </LinkContainer>
+            <LinkContainer to='/login' >
+              <NavItem eventKey={3} > Login </NavItem>
+            </LinkContainer>
+         </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   </div>
 )
 
